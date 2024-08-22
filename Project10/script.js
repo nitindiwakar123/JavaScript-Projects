7f8e46eb577c83c3d50dd83498514a96a1330c81
nitindiwakar123 <nitindiwakar2006@gmail.com>
2024-08-22 22:51:25 +0530

    first commit

---
+++ Project10/script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('#btn');
+const ul = document.querySelector('ul');
+
+button.addEventListener('click', function (e) {
+    // if (ul.classList.contains('sm')) {
+    //     ul.classList.remove('sm');
+
+    // }
+    // else{
+    //     ul.classList.remove('links')
+    //     ul.classList.add('sm');
+    // }
+
+    ul.classList.toggle('sm')
+})
---
+++ Project20/script.js
@@ -0,0 +1,5 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scrollend', () => {
+    nav.style.backgroundColor = "green";
+})
